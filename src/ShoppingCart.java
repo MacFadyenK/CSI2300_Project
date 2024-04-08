d5e0fa3aebadf5a1e9cf5016959ef86bc0cf0544
MacFadyenK <khmacfadyen@oakland.edu>
2024-04-08 12:50:20 -0400

    removed unused methods

--- src/ShoppingCart.java
+++ src/ShoppingCart.java
@@ -1,14 +1,11 @@
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class ShoppingCart {
     private List<Item> items;
-    private Scanner scanner;
 
     public ShoppingCart() {
         this.items = new ArrayList<>();
-        this.scanner = new Scanner(System.in);
     }
 
     // Method to add an item to the cart
@@ -25,18 +22,6 @@ public class ShoppingCart {
             count += 1*i.getQuantity();
         }
         return count;
-    }
-
-    // Method to choose size for the item
-    private String chooseSize(Item item) {
-        System.out.println("Enter size for " + item.getName() + ": ");
-        return scanner.nextLine();
-    }
-
-    // Method to choose color for the item
-    private String chooseColor(Item item) {
-        System.out.println("Enter color for " + item.getName() + ": ");
-        return scanner.nextLine();
     }
 
     //gets the arraylist for all items in cart
@@ -68,14 +53,5 @@ public class ShoppingCart {
             i.inCart = false;
         }
         items.clear();
-    }
-  
-
-    public static void main(String[] args) {
-        ShoppingCart cart = new ShoppingCart();
-
-
-        // Displaying the cart contents
-        cart.displayCart();
     }
 }
